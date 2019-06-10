9914d33ff8684b6f622a2fd82d9d5c395c315680
Mosdetektiv <mosdetektiv@inbox.ru>
2019-06-10 11:50:46 +0300

    Add projects,  convertor and parser

---
+++ ParsQsh/ParsQsh/Program.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using System.Drawing;
+using System.IO;
+
+namespace ParsQsh
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            IWebDriver Browser;
+            Browser = new OpenQA.Selenium.Chrome.ChromeDriver();
+            string pathQshSave = @"C:\Users\mmzhi\Downloads\qsh";
+
+
+            //get old folder contain .qsh
+            string[] foldersQsh = Directory.GetDirectories(pathQshSave);
+            for (int i = 0; i < foldersQsh.Length; i++)
+            {
+                try
+                {
+                    foldersQsh[i] = foldersQsh[i].Replace(pathQshSave + "\\", "");
+                }
+                catch (Exception ex)
+                {
+
+                    Console.WriteLine(ex.Message);
+                }
+            }
+
+            //go in browser
+            Browser.Navigate().GoToUrl("http://erinrv.qscalp.ru/");
+            System.Threading.Thread.Sleep(2000);
+
+            //get list dates for qsh 
+            ReadOnlyCollection<IWebElement> SearchDates = Browser.FindElements(By.XPath("/html/body/pre/a"));
+            List<dayUrl> DatesUrl = new List<dayUrl>();
+            foreach (var date in SearchDates)
+            {
+                if ( Array.IndexOf(foldersQsh, date.Text)<0)
+                {
+                    DatesUrl.Add(new dayUrl("http://erinrv.qscalp.ru/" + date.Text, date.Text));
+                }
+            }
+
+            //get history qsh
+            foreach (var DateUrl in DatesUrl)
+            {
+
+                Browser.Navigate().GoToUrl(DateUrl.url);
+                System.Threading.Thread.Sleep(2000);
+               
+                ReadOnlyCollection<IWebElement> equitys = Browser.FindElements(By.XPath("/html/body/pre/a"));
+                List<equityURL> equitysURLs = new List<equityURL>();
+                
+                for (int i = 1; i < equitys.Count; i++)
+                {
+                    equitysURLs.Add(new equityURL(DateUrl.url +"/" + equitys[i].Text, equitys[i].Text ));
+                }
+             //prepare folder for moving
+                if (!Directory.Exists(pathQshSave + "\\" + DateUrl.day))
+                {
+                    Directory.CreateDirectory(pathQshSave + "\\" + DateUrl.day);
+                }
+             //download
+                foreach (var equityURL in equitysURLs)
+                {
+                    try
+                    {
+                        Browser.FindElement(By.LinkText(equityURL.nameFolder)).Click();
+                        System.Threading.Thread.Sleep(1000);
+                        File.Move(pathQshSave.Replace("qsh","") + equityURL.nameFolder, pathQshSave + "\\" + DateUrl.day + "\\" + equityURL.nameFolder);
+                    }
+                    catch (Exception exc)
+                    {
+
+                        Console.WriteLine(exc.Message);
+                    }
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+    struct equityURL
+    {
+        public string pathUrl { get; set; }
+        public string nameFolder { get; set; }
+        public equityURL(string pathUrl, string nameFolder)
+        {
+            this.pathUrl = pathUrl;
+            this.nameFolder = nameFolder;
+        }
+    }
+    struct dayUrl
+    {
+        public string url { get; set; }
+        public string day { get; set; }
+        public dayUrl(string url, string day)
+        {
+            this.url = url;
+            this.day = day;
+        }
+    }
+ }
---
+++ QshToCSV/QshToCSV/Program.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using QScalp;
+using QScalp.History;
+using QScalp.History.Reader;
+
+namespace QshToCSV
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Settings set = new Settings();
+            //set.ReadSettings();
+            //ConvertFile(fileName, format);
+            WriteY();
+            Console.WriteLine("ASD");
+
+            Console.WriteLine("vAH");
+            while (true)
+                Console.Write("x"); // Все время печатать 'x'
+
+            Console.ReadKey();
+        }
+        static void WriteY()
+        {
+            while (true)
+                Console.Write("y"); // Все время печатать 'y'
+        }
+
+
+        private static void ConvertFile(string fileName, string format)
+        {
+            List<string> Quotes = new List<string>();
+
+            var fileNameKey = format + "_" + fileName;
+
+            //read .qsh file
+            using (var qr = QshReader.Open(fileName))
+            {
+                var reader = qr;
+                var currentDate = qr.CurrentDateTime;
+
+                for (var i = 0; i < qr.StreamCount; i++)
+                {
+                    var stream = (ISecurityStream)qr[i];
+
+                    switch (stream.Type)
+                    {
+                        case StreamType.Quotes:
+                            {
+                                    ((IQuotesStream)stream).Handler += quotes =>
+                                    {
+                                        string s = "";
+
+                                        foreach (var item in quotes)
+                                        {
+                                            s = s + "||" + item.Price.ToString() +";" + item.Type.ToString() + ";" + item.Volume.ToString();
+                                        }
+                                        s = s.Remove(0,2) + "||" + qr.CurrentDateTime.ToString();
+                                        Quotes.Add(s);
+                                        
+                                    };
+                                break;
+                            }
+                        case StreamType.Deals:
+                            {
+                                //    ((IDealsStream)stream).Handler += deal =>
+                                //    {
+                                //        secData.Item2.Add(new ExecutionMessage
+                                //        {
+                                //            LocalTime = reader.CurrentDateTime.ApplyTimeZone(TimeHelper.Moscow),
+                                //            HasTradeInfo = true,
+                                //            ExecutionType = ExecutionTypes.Tick,
+                                //            SecurityId = securityId,
+                                //            OpenInterest = deal.OI == 0 ? (long?)null : deal.OI,
+                                //            ServerTime = deal.DateTime.ApplyTimeZone(TimeHelper.Moscow),
+                                //            TradeVolume = deal.Volume,
+                                //            TradeId = deal.Id == 0 ? (long?)null : deal.Id,
+                                //            TradePrice = deal.Price,
+                                //            OriginSide =
+                                //                deal.Type == DealType.Buy
+                                //                    ? Sides.Buy
+                                //                    : (deal.Type == DealType.Sell ? Sides.Sell : (Sides?)null)
+                                //        });
+
+                                //        TryFlushData(registry, security, format, ExecutionTypes.Tick, secData.Item2, reader);
+                                //    };
+                                break;
+                            }
+                        default:
+                            throw new ArgumentOutOfRangeException("Неподдерживаемый тип потока {0}.");
+                    }
+                }
+
+                while (qr.CurrentDateTime != DateTime.MaxValue)
+                { qr.Read(true); }
+
+            }
+            TryFlushData(Quotes);
+        }
+        //record file result
+        private static void TryFlushData(List<string> Quotes)
+        {
+            try
+            {
+                using (var str = new FileStream(Path.Combine(Environment.CurrentDirectory, "result.csv"), FileMode.Create, FileAccess.Write))
+                {
+                    using (var wr = new StreamWriter(str))
+                    {
+                        foreach (string st in Quotes)
+                        {
+                            wr.WriteLine(st);
+                        }
+                        wr.Close();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
