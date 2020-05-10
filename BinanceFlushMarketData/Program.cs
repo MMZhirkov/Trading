10c4b7f9e4cd79544cdaafa18c34cf6b6f0be8f3
mmzhirkov <mmzhirkov@yandex.ru>
2020-05-11 02:42:52 +0300

    Start project flush marketData

---
+++ BinanceFlushMarketData/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BinanceFlushMarketData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
